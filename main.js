ad0e95ed948c4e9f4519fd9c81f01c13779626e2
DRAK0N41K <d.andriychak@gmail.com>
2017-05-22 23:21:22 +0300

    2th_comm

--- main.js
+++ main.js
@@ -4,21 +4,22 @@ console.log(arr);
 arr.length = 0;
 console.log(arr);
 // 2
-var arr1 [1,2,3,4,5,6,7,8,9,0];
-arr1.join(',');
+var arr1 = [1,2,3,4,5,6,7,8,9,0];
+arr1.reverse();
 console.log(arr1);
 // 3
 var  fruits = ["Banana", "Orange", "Apple", "Mango"];
-for(var i = 0; i < 3; i++){
+for(var i = 0; i <= 3; i++){
   console.log(fruits[i]);
 }
 // 4
 var start = [100];
-var result = [0];
-for(var x = 1; x <= 100; x++) {
-  result.push(x);
+var result = [100];
+for(; start >= 0; start--) {
+  result.push(start);
 }
 // 5
 var arr2 = ['lol','kek','chebyraska'];
-arr2.splice(2,1,'chebyrec');
+arr2.pop();
+arr2.push('chebyreck');
 console.log(arr2);
