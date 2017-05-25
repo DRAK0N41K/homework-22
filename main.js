7730f1faaacd6de100c720e3b62b8a5199e25ddb
DRAK0N41K <d.andriychak@gmail.com>
2017-05-25 23:13:00 +0300

    teper mae bytu norm

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ arr1.reverse();
 console.log(arr1);
 // 3
 var  fruits = ["Banana", "Orange", "Apple", "Mango"];
-var howmany = (fruits.length)
+var howmany = (fruits.length - 1)
 for(var i = 0; i <= howmany; i++){
   console.log(fruits[i]);
 }
 // 4
-var start = [100];
+var start = 100;
 var result = [100];
 for(; start >= 0; start--) {
   result.push(start);
 }
 // 5
-var arr2 = ['lol','kek','chebyraska'];
-arr2.splice(-1, 1, 'chebureck')
+var arr2 = ['lol','kek','chebyraska', 'hah', 'bez', 'lola', 'i', 'keka', 'nema', 'chebyreka'];
+arr2.splice(-2, 1, 'chebureck');
 console.log(arr2);
